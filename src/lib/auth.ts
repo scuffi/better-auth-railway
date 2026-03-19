2b5532531823828aa61018b1e35841327fe72416
Archie Ferguson <45369682+scuffi@users.noreply.github.com>
2026-03-19 03:01:00 +0000

    add multiple plugins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,53 +1,55 @@
 import { betterAuth } from "better-auth";
 import { openAPI } from "better-auth/plugins";
 import { Pool } from "pg";
-import { Redis } from "ioredis"
+import { Redis } from "ioredis";
+import { dash } from "@better-auth/infra";
+import { passkey } from "@better-auth/passkey";
 
 const redis = new Redis(`${process.env.REDIS_URL}?family=0`)
-   .on("error", (err) => {
-     console.error("Redis connection error:", err)
-   })
-   .on("connect", () => {
-     console.log("Redis connected")
-   })
+  .on("error", (err) => {
+    console.error("Redis connection error:", err);
+  })
+  .on("connect", () => {
+    console.log("Redis connected");
+  })
   .on("ready", () => {
-     console.log("Redis ready")
-   })
+    console.log("Redis ready");
+  });
 
 // Check better-auth docs for more info https://www.better-auth.com/docs/
 export const auth = betterAuth({
-	emailAndPassword: {
-		enabled: true,
-	},
-	// Session config
-	session: {
-		cookieCache: {
-			enabled: true,
-			maxAge: 5 * 60,
-		},
-	},
-	// Add your plugins here
-	plugins: [openAPI()],
-	// DB config
-	database: new Pool({
-		connectionString: process.env.DATABASE_URL,
-		log: console.log,
-	}),
-	// This is for the redis session storage
-	secondaryStorage: {
-		get: async (key) => {
-			const value = await redis.get(key);
-			return value ? value : null;
-		},
-		set: async (key, value, ttl) => {
-			if (ttl) {
-				await redis.set(key, value, "EX", ttl);
-			} else {
-				await redis.set(key, value);
-			}
-		},
-		delete: async (key) => {
-			await redis.del(key);
-		},
-	},
+  emailAndPassword: {
+    enabled: true,
+  },
+  // Session config
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60,
+    },
+  },
+  // Add your plugins here
+  plugins: [openAPI(), dash(), passkey()],
+  // DB config
+  database: new Pool({
+    connectionString: process.env.DATABASE_URL,
+    log: console.log,
+  }),
+  // This is for the redis session storage
+  secondaryStorage: {
+    get: async (key) => {
+      const value = await redis.get(key);
+      return value ? value : null;
+    },
+    set: async (key, value, ttl) => {
+      if (ttl) {
+        await redis.set(key, value, "EX", ttl);
+      } else {
+        await redis.set(key, value);
+      }
+    },
+    delete: async (key) => {
+      await redis.del(key);
+    },
+  },
 });
